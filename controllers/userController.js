6b0e495fad939348824bb7cec9651d5b606959ff
Sanjaiweb <sanjaisanjai7182@gmail.com>
2025-01-22 22:00:09 +0530

    `Added JWT authentication for user login and protected current user route with token validation.`

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 //@desc Register a new user
 //@route POST /api/users/register
 //@access Public
@@ -35,8 +36,30 @@ const registeruser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //@access Public
 const loginuser = asyncHandler(async (req, res) => {
+    const {email , password} = req.body;
+    if(!email||!password){
+        res.status(400);
+        throw new Error("Please Fill All Fields");
+    }
+    const user = await User.findOne({email});
+//compare password with hashed password
+    if (user && (await bcrypt.compare(password, user.password))) {
+       const accessToken = jwt.sign({
+        user:{
+            username : user.username,
+            email : user.email,
+            id : user._id,
 
-    res.json({ message: "Login Route" });
+        }
+       },process.env.ACCESS_TOKEN_SECRET, 
+         {expiresIn: "1m"}
+    );res.status(200).json({ accessToken });
+    }
+    else{
+        res.status(401);
+        throw new Error("Invalid Password or Invalid Email");
+    }
+   
 });
 //@desc Get current user
 //@route GET /api/users/current
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { registeruser, loginuser,getcurrentuser} = require('../controllers/userController');
 const router = express.Router();
+const {validateToken} = require('../middleware/validateTokenHandler');
 
 router.post('/register',registeruser);
 router.post('/login', loginuser);
-router.get('/current', getcurrentuser);
+router.get('/current', validateToken,getcurrentuser);
 
 module.exports = router;
